7580fe3b41ea3b021c0a575773687233ae9c21af
villeantropist <villeantropist@gmail.com>
2023-09-08 17:52:21 +0000

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This is the "0-add_integer" module.
+"""
+This is the "0-add_integer" module.
 The 0-add_integer module supplies one function, add_integer(a, b).
 """
 
@@ -15,3 +16,4 @@ def add_integer(a, b):
     if type(b) is float:
         b = int(b)
     return a + b
+
